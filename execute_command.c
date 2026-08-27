36f18ef7fb58a0e9496fb7524da491007bfe5030
Khaled Alyhya <khaled.alyhya10@gmail.com>
2026-08-27 15:43:14 +0300

    Handle command arguments

--- execute_command.c
+++ execute_command.c
@@ -2,16 +2,29 @@
 
 /**
  * execute_command - Executes a command in a child process
- * @line: Command to execute
+ * @line: Command line to execute
  * @program_name: Name of the shell program
  */
 void execute_command(char *line, char *program_name)
 {
-	char *argv[2];
+	char *args[64];
+	char *token;
+	int i = 0;
 	pid_t pid;
 
-	argv[0] = line;
-	argv[1] = NULL;
+	token = strtok(line, " \t");
+
+	while (token != NULL && i < 63)
+	{
+		args[i] = token;
+		i++;
+		token = strtok(NULL, " \t");
+	}
+
+	args[i] = NULL;
+
+	if (args[0] == NULL)
+		return;
 
 	pid = fork();
 
@@ -23,7 +36,7 @@ void execute_command(char *line, char *program_name)
 
 	if (pid == 0)
 	{
-		if (execve(line, argv, environ) == -1)
+		if (execve(args[0], args, environ) == -1)
 		{
 			perror(program_name);
 			exit(EXIT_FAILURE);
--- main.h
+++ main.h
@@ -6,6 +6,7 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <string.h>
 
 extern char **environ;
 
