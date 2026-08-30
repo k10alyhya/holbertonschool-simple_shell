172f92fb138cf4bc784328f318f89b2947e5256b
Khaled Alyhya <khaled.alyhya10@gmail.com>
2026-08-30 13:09:38 +0300

    Refactor command execution

--- execute_command.c
+++ execute_command.c
@@ -14,7 +14,78 @@ void print_error(char *prog, char *cmd)
 }
 
 /**
- * execute_command - Executes a command or built-in in a child process
+ * handle_builtin - Handles shell built-in commands
+ * @args: Command arguments
+ * @line: Input buffer
+ * @last_status: Status of the last command
+ *
+ * Return: 1 if a built-in was handled, otherwise 0
+ */
+int handle_builtin(char **args, char *line, int *last_status)
+{
+	int i;
+
+	if (strcmp(args[0], "exit") == 0)
+	{
+		free(line);
+		exit(*last_status);
+	}
+
+	if (strcmp(args[0], "env") == 0)
+	{
+		for (i = 0; environ[i] != NULL; i++)
+		{
+			write(STDOUT_FILENO, environ[i], strlen(environ[i]));
+			write(STDOUT_FILENO, "\n", 1);
+		}
+		*last_status = 0;
+		return (1);
+	}
+
+	return (0);
+}
+
+/**
+ * run_command - Runs an executable command
+ * @args: Command arguments
+ * @path: Full executable path
+ * @line: Input buffer
+ * @last_status: Status of the last command
+ */
+void run_command(char **args, char *path, char *line, int *last_status)
+{
+	pid_t pid;
+	int status;
+
+	pid = fork();
+
+	if (pid == -1)
+	{
+		perror("fork");
+		free(path);
+		return;
+	}
+
+	if (pid == 0)
+	{
+		if (execve(path, args, environ) == -1)
+		{
+			free(path);
+			free(line);
+			exit(EXIT_FAILURE);
+		}
+	}
+
+	wait(&status);
+
+	if (WIFEXITED(status))
+		*last_status = WEXITSTATUS(status);
+
+	free(path);
+}
+
+/**
+ * execute_command - Executes a command or built-in
  * @line: Command line to execute
  * @program_name: Name of the shell program
  */
@@ -23,75 +94,30 @@ void execute_command(char *line, char *program_name)
 	char *args[64];
 	char *token;
 	char *actual_path;
-	int i = 0, j, status;
 	static int last_status;
-	pid_t pid;
+	int i = 0;
 
 	token = strtok(line, " \t");
+
 	while (token != NULL && i < 63)
 	{
-		args[i] = token;
-		i++;
+		args[i++] = token;
 		token = strtok(NULL, " \t");
 	}
+
 	args[i] = NULL;
 
-	if (args[0] == NULL)
+	if (args[0] == NULL || handle_builtin(args, line, &last_status))
 		return;
-
-	if (strcmp(args[0], "exit") == 0)
-	{
-		free(line);
-		exit(last_status);
-	}
-
-	if (strcmp(args[0], "env") == 0)
-	{
-		for (j = 0; environ[j] != NULL; j++)
-		{
-			write(STDOUT_FILENO, environ[j], strlen(environ[j]));
-			write(STDOUT_FILENO, "\n", 1);
-		}
-		last_status = 0;
-		return;
-	}
 
 	actual_path = get_path(args[0]);
+
 	if (actual_path == NULL)
 	{
 		print_error(program_name, args[0]);
 		last_status = 127;
-		if (!isatty(STDIN_FILENO))
-		{
-			free(line);
-			exit(127);
-		}
 		return;
 	}
 
-	pid = fork();
-	if (pid == -1)
-	{
-		perror("fork");
-		free(actual_path);
-		return;
-	}
-
-	if (pid == 0)
-	{
-		if (execve(actual_path, args, environ) == -1)
-		{
-			perror(program_name);
-			free(actual_path);
-			free(line);
-			exit(EXIT_FAILURE);
-		}
-	}
-	else
-	{
-		wait(&status);
-		if (WIFEXITED(status))
-			last_status = WEXITSTATUS(status);
-		free(actual_path);
-	}
+	run_command(args, actual_path, line, &last_status);
 }
--- main.h
+++ main.h
@@ -6,18 +6,18 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <string.h>
-#include <sys/types.h>
 #include <sys/stat.h>
-#include <unistd.h>
+#include <string.h>
 
 extern char **environ;
 
 void execute_command(char *line, char *program_name);
+void print_error(char *prog, char *cmd);
+int handle_builtin(char **args, char *line, int *last_status);
+void run_command(char **args, char *path, char *line, int *last_status);
 char *trim_spaces(char *line);
 char *build_full_path(char *dir, char *command);
 char *_getenv(const char *name);
 char *get_path(char *command);
-
 
 #endif
